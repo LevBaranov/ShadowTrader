e9f2999c25ec29ca30ed1b44a022218613b8fd25
LevBaranov <lev.l.baranov@gmail.com>
2025-05-07 17:57:19 +0500

    Сообщение с текущим балансом

--- bot/callbacks.py
+++ bot/callbacks.py
@@ -6,7 +6,7 @@ from aiogram.types import CallbackQuery
 from core.portfolio_manager import PortfolioManager
 
 from bot.utils import AccountCallbackFactory
-from bot.texts import action
+from bot.texts import action, free_cash, position
 
 from config import settings
 
@@ -22,6 +22,10 @@ async def callbacks_account(
     account_id = callback_data.value
     manager = PortfolioManager(account_id)
     portfolio = manager.get_portfolio()
+    message = (Template(free_cash).substitute(cash=portfolio.cash.to_float()) +
+               "\n".join([Template(position).substitute(ticker=pos.ticker, balance=pos.balance)
+                          for pos in portfolio.shares]))
+    await callback.message.answer(message)
     index_moex = manager.get_index_list(settings.stock_market.index_name)
 
     actions, cash = manager.get_action_for_rebalance(portfolio, index_moex)
--- bot/texts.py
+++ bot/texts.py
@@ -1,4 +1,6 @@
 welcome = """Приветственный привет!
 Кол-во счетов обнаружено: $accounts_count
 """
-action = """Действие: $type, Акция: $ticker, Кол-во: $count"""
+action = "Действие: $type, Акция: $ticker, Кол-во: $count"
+free_cash = "Текущие свободные средства: $cash \n"
+position = "$ticker: $balance шт"
